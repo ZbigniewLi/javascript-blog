bdf0fa960625519c4a7c3132e8bcdbcf1cd738fc
Zbigniew Liskiewicz <zbigniew.jeremi.l@gmail.com>
2023-08-29 10:22:52 +0200

    add function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
-
 const titleClickHandler = function(event){
+    const clickedElement = this
     console.log('Link was clicked!');
     event.preventDefault();
   
@@ -13,30 +13,45 @@ for(let activeLink of activeLinks){
   
     /* add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active')
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
-
-    for(let activeLink of activeLinks){
-      activeLink.classList.add('active');
-    }
   
     /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.articles a.active');
+    const activeArticles = document.querySelectorAll('.posts .post.active');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
   
     /* get 'href' attribute from the clicked link */
-    
+const href = clickedElement.getAttribute('href')
+
   
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(href)
   
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active')
   }
   
-  const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+ 
+  const generateTitleLinks = function () {
+   const titles = document.querySelector('.titles');
+   titles.innerHTML = ''
+   const articles = document.querySelectorAll('.post')
+   for(let article of articles){
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector('.post-title').innerHTML
+    const html = '<li><a href="#'+articleId+'"><span>'+articleTitle+'</span></a></li>'
+    titles.innerHTML = titles.innerHTML + html
+    
+   }
+   const links = document.querySelectorAll('.titles a');
+  
+   for(let link of links){
+     link.addEventListener('click', titleClickHandler);
+   }
+ 
   }
+  generateTitleLinks()
